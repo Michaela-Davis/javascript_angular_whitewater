16a2b37d94f570313e6361f46dad60e1a4d5c6b7
Michaela Davis <michaela.delight@gmail.com>
2017-04-07 10:07:23 -0700

    create rafter.model

---
+++ src/app/rafter.model.ts
@@ -0,0 +1,9 @@
+export class Rafter {
+  constructor(
+    public name: string,
+    public email: string,
+    public state: string,
+    public yearsRafting: number,
+    public favoriteRiverStretch: string,
+    public guide: string) { }
+}
